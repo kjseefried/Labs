9ddb0c28545e8d9b198f0aa617cd2abb0e0c9904
Alex <alex.aanis@gmail.com>
2014-01-28 15:43:31 +0100

    Two way communication

--- Computer network/lab1/server.c
+++ Computer network/lab1/server.c
@@ -12,14 +12,16 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <string.h>
 
 #define PORT 5555
 #define MAXMSG 512
+#define messageLength  256
 
 /* makeSocket
  * Creates and names a socket in the internet
  * namespace. The socket created exists
- * on the machine from which the function is 
+ * on the machine from which the function is
  * called. Instead of finding and using the
  * machine's internet address, the function
  * specifies INADDR_ANY as the host address;
@@ -27,32 +29,32 @@
  * actual address.
  */
 int makeSocket(unsigned short int port) {
-  int sock;
-  struct sockaddr_in name;
-
-  /* Create a socket. */
-  sock = socket(PF_INET, SOCK_STREAM, 0);
-  if(sock < 0) {
-    perror("Could not create a socket\n");
-    exit(EXIT_FAILURE);
-  }
-  /* Give the socket a name. */
-  /* Socket address format set to AF_INET for internet use. */
-  name.sin_family = AF_INET;
+	int sock;
+	struct sockaddr_in name;
+	
+	/* Create a socket. */
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	if(sock < 0) {
+		perror("Could not create a socket\n");
+		exit(EXIT_FAILURE);
+	}
+	/* Give the socket a name. */
+	/* Socket address format set to AF_INET for internet use. */
+	name.sin_family = AF_INET;
     /* Set port number. The function htons converts from host byte order to network byte order.*/
-  name.sin_port = htons(port);
-  /* Set the internet address of the host the function is called from. */
-  /* The function htonl converts INADDR_ANY from host byte order to network byte order. */
-  /* (htonl does the same thing as htons but the former converts a long integer whereas
-   * htons converts a short.) 
-   */
-  name.sin_addr.s_addr = htonl(INADDR_ANY);
-  /* Assign an address to the socket by calling bind. */
-  if(bind(sock, (struct sockaddr *)&name, sizeof(name)) < 0) {
-    perror("Could not bind a name to the socket\n");
-    exit(EXIT_FAILURE);
-  }
-  return(sock);
+	name.sin_port = htons(port);
+	/* Set the internet address of the host the function is called from. */
+	/* The function htonl converts INADDR_ANY from host byte order to network byte order. */
+	/* (htonl does the same thing as htons but the former converts a long integer whereas
+	 * htons converts a short.)
+	 */
+	name.sin_addr.s_addr = htonl(INADDR_ANY);
+	/* Assign an address to the socket by calling bind. */
+	if(bind(sock, (struct sockaddr *)&name, sizeof(name)) < 0) {
+		perror("Could not bind a name to the socket\n");
+		exit(EXIT_FAILURE);
+	}
+	return(sock);
 }
 
 /* readMessageFromClient
@@ -60,77 +62,97 @@ int makeSocket(unsigned short int port) {
  * denoted by the file descriptor 'fileDescriptor'.
  */
 int readMessageFromClient(int fileDescriptor) {
-  char buffer[MAXMSG];
-  int nOfBytes;
+	char buffer[MAXMSG];
+	int nOfBytes;
+	
+	nOfBytes = read(fileDescriptor, buffer, MAXMSG);
+	if(nOfBytes < 0) {
+		perror("Could not read data from client\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+		if(nOfBytes == 0)
+		/* End of file */
+			return(-1);
+		else
+		/* Data read */
+			printf(">Incoming message: %s\n",  buffer);
+	return(0);
+}
 
-  nOfBytes = read(fileDescriptor, buffer, MAXMSG);
-  if(nOfBytes < 0) {
-    perror("Could not read data from client\n");
-    exit(EXIT_FAILURE);
-  }
-  else
-    if(nOfBytes == 0) 
-      /* End of file */
-      return(-1);
-    else 
-      /* Data read */
-      printf(">Incoming message: %s\n",  buffer);
-  return(0);
+
+/* writeMessage
+ * Writes the string message to the file (socket)
+ * denoted by fileDescriptor.
+ */
+void writeMessage(int fileDescriptor, char *message) {
+	int nOfBytes;
+	nOfBytes = write(fileDescriptor, message, strlen(message) + 1);
+	if(nOfBytes < 0) {
+		perror("writeMessage - Could not write data\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
 int main(int argc, char *argv[]) {
-  int sock;
-  int clientSocket;
-  int i;
-  fd_set activeFdSet, readFdSet; /* Used by select */
-  struct sockaddr_in clientName;
-  size_t size;
-  
- 
-  /* Create a socket and set it up to accept connections */
-  sock = makeSocket(PORT);
-  /* Listen for connection requests from clients */
-  if(listen(sock,1) < 0) {
-    perror("Could not listen for connections\n");
-    exit(EXIT_FAILURE);
-  }
-  /* Initialise the set of active sockets */
-  FD_ZERO(&activeFdSet);
-  FD_SET(sock, &activeFdSet);
-  
-  printf("\n[waiting for connections...]\n");
-  while(1) {
-    /* Block until input arrives on one or more active sockets
-       FD_SETSIZE is a constant with value = 1024 */
-    readFdSet = activeFdSet;
-    if(select(FD_SETSIZE, &readFdSet, NULL, NULL, NULL) < 0) {
-      perror("Select failed\n");
-      exit(EXIT_FAILURE);
-    }
-    /* Service all the sockets with input pending */
-    for(i = 0; i < FD_SETSIZE; ++i) 
-      if(FD_ISSET(i, &readFdSet)) {
-	if(i == sock) {
-	  /* Connection request on original socket */
-	  size = sizeof(clientName);
-	  /* Accept the connection request from a client. */
-	  clientSocket = accept(sock, (struct sockaddr *)&clientName, &size); 
-	  if(clientSocket < 0) {
-	    perror("Could not accept connection\n");
-	    exit(EXIT_FAILURE);
-	  }
-	  printf("Server: Connect from client %s, port %hd\n", 
-		 inet_ntoa(clientName.sin_addr), 
-		 ntohs(clientName.sin_port));
-	  FD_SET(clientSocket, &activeFdSet);
+	int sock;
+	int clientSocket;
+	int i;
+	fd_set activeFdSet, readFdSet; /* Used by select */
+	struct sockaddr_in clientName;
+	size_t size;
+	char messageString[messageLength];
+	
+	/* Create a socket and set it up to accept connections */
+	sock = makeSocket(PORT);
+	/* Listen for connection requests from clients */
+	if(listen(sock,1) < 0) {
+		perror("Could not listen for connections\n");
+		exit(EXIT_FAILURE);
 	}
-	else {
-	  /* Data arriving on an already connected socket */
-	  if(readMessageFromClient(i) < 0) {
-	    close(i);
-	    FD_CLR(i, &activeFdSet);
-	  }
+	/* Initialise the set of active sockets */
+	FD_ZERO(&activeFdSet);
+	FD_SET(sock, &activeFdSet);
+	
+	printf("\n[waiting for connections...]\n");
+	while(1) {
+		/* Block until input arrives on one or more active sockets
+		 FD_SETSIZE is a constant with value = 1024 */
+		readFdSet = activeFdSet;
+		if(select(FD_SETSIZE, &readFdSet, NULL, NULL, NULL) < 0) {
+			perror("Select failed\n");
+			exit(EXIT_FAILURE);
+		}
+		/* Service all the sockets with input pending */
+		for(i = 0; i < FD_SETSIZE; ++i)
+			if(FD_ISSET(i, &readFdSet)) {
+				if(i == sock) {
+					/* Connection request on original socket */
+					size = sizeof(clientName);
+					/* Accept the connection request from a client. */
+					clientSocket = accept(sock, (struct sockaddr *)&clientName, &size);
+					if(clientSocket < 0) {
+						perror("Could not accept connection\n");
+						exit(EXIT_FAILURE);
+					}
+					printf("Server: Connect from client %s, port %hd\n",
+						   inet_ntoa(clientName.sin_addr),
+						   ntohs(clientName.sin_port));
+					FD_SET(clientSocket, &activeFdSet);
+				}
+				else {
+					/* Data arriving on an already connected socket */
+					if(readMessageFromClient(i) < 0) {
+						close(i);
+						FD_CLR(i, &activeFdSet);
+					}
+					printf("\n>");
+					fgets(messageString, messageLength, stdin);
+					messageString[messageLength - 1] = '\0';
+					writeMessage(i, messageString);
+					
+					
+				}
+			}
 	}
-      }
-  }
 }
